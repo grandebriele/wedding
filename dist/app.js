c00592b1276cea1dfcdf1f7060a707712a1bf90e
mercury <gabrielemercurio84@gmail.com>
2021-05-01 15:05:34 +0200

    Update app.js and app.js

--- dist/app.js
+++ dist/app.js
@@ -1401,25 +1401,6 @@ $(document).ready(function () {
   $('.modal-menu-wrap .regalo-modal').click(function () {
     $('.nav-menu-wrapper, #hero, main, footer').show();
     $(".modal-menu-wrap").removeClass("active");
-  }); // *** ANIMATION ON SCROLL ANCHOR <a>
-  // Add smooth scrolling to all <a> links
-
-  $("header li a").on('click', function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault(); // Store hash
-
-      var hash = this.hash; // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top - 90
-      }, function () {// Add hash (#) to URL when done scrolling (default click behavior)
-        // window.location.hash = hash;
-      });
-    } // End if
-
   });
 }); // END
 
